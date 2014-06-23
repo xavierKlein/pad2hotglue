3eeb641d7160511e86ee5e5cf31c1a7c835d18bf
christop <christoph@lafkon.net>
2014-06-23 16:21:20 +0200

    don't call it a rewrite

--- pad2hotglue.sh
+++ pad2hotglue.sh
@@ -1,119 +1,118 @@
 #!/bin/bash
 
- ACCESS=`head -n 1 conf/ftp.conf`
- HOST=`tail -n 1 conf/ftp.conf`
-
- HOTGLUECONTENT=hotglue/content/test1/head
-
- YPOS=50
- WIDTH=550
- HEIGHT=0
-
- TMPDIR=tmp
- rm $TMPDIR/*.md
-
- echo "$ACCESS" >ftp.temp
- echo "delete $hotglueContent/1*" >> ftp.temp
-
-
- PADDUMP=pad.md
- PADURL=http://note.pad.constantvzw.org:8000/p/interviews.tools-of-the-trade/export/txt
- PADHTML=pad.html
-
- wget --no-check-certificate -O $PADDUMP  $PADURL
-
-# pandoc pad.md -f markdown -t html -s -o pad.html
-# cat $PADDUMP | grep -v ^% | pandoc -r markdown -w html >> $PADHTML
-
-# grep "% NOWSPEAKING:" pad.md | cut -d ":"  -f2 |  sed 's/ //g'| sort | uniq 
-
-
- FILE=1000
- if [ -f $FILE ]; then rm $FILE ; fi
-
-
-
-
-
- for LINE in `cat $PADDUMP | sed 's/ /djqteDF34/g'`
-  do
+  ACCESS=`head -n 1 conf/ftp.conf`
+    HOST=`tail -n 1 conf/ftp.conf`
  
-     LINE=`echo $LINE | sed 's/djqteDF34/ /g'`
-     CHECK=$(echo "$LINE" |grep "% NOWSPEAKING:" | wc -l )
+  HOTGLUECONTENT=hotglue/content/test1/head
+
+# START VALUES
+  XPOS=20 ; YPOS=50
+  WIDTH=550 ;  HEIGHT=0
+ 
+  TMPDIR=tmp
+  FTPTMP=$TMPDIR/ftp.input
+  rm $TMPDIR/*.md
+ 
+# UPLOAD VIA FTP (CONTROL FILE USED LATER)
+  echo "$ACCESS"                    >  $FTPTMP
+  echo "mdelete $HOTGLUECONTENT/1*" >> $FTPTMP
+ 
+ 
+  PADDUMP=$TMPDIR/pad.md
+  PADURL=http://note.pad.constantvzw.org:8000/p/interviews.tools-of-the-trade/export/txt
+  PADHTML=${PADDUMP%%.*}.html
+
+# DOWNLOAD THE PAD
+  wget --no-check-certificate -O $PADDUMP  $PADURL
+ 
+  FILENAME=1000
+  FILE=$TMPDIR/$FILENAME
+  if [ -f $FILE ]; then rm $FILE ; fi
 
 
+
+# --------------------------------------------------------------------------- #
+# SPLIT THE CONTENT IN SEPARATE PARTS FOR SPEAKERS
+# --------------------------------------------------------------------------- #
+  for LINE in `cat $PADDUMP | sed 's/ /djqteDF34/g'`
+   do
+  
+    # RESTORE SPACES ON CURRENT LINE
+      LINE=`echo $LINE | sed 's/djqteDF34/ /g'`
+ 
+    # WRITE LINE TO TEMPORARY FILE
+      echo "$LINE"  >>  ${FILE}.box
+ 
+    # CHECK IF SPEAKER CHANGED
+      CHECK=$(echo "$LINE" |grep "% NOWSPEAKING:" | wc -l )
+ 
+    # IF THERE IS A DIFFERENT SPEAKER 
       if [ $CHECK -gt 0 ]
        then
  	
-YPOS=$(python -c "print ${YPOS}+${HEIGHT}-40")
-NBRCARAC=$(wc -m < "$FILE")
-LINENUMBER=$(python -c "print ${NBRCARAC}/65")
-HEIGHT=$(python -c "print ${LINENUMBER}*15")
-BOXCOLOR=255,255,255
-
-MAXPERLINE=70
-
-echo "type:text"  											>  $FILE
-echo "module:text"  											>> $FILE
-echo "object-height:${HEIGHT}px"    							>>  $FILE
-echo "object-left:${XPOS}px"									>>  $FILE
-echo "object-top:${YPOS}px" 										>>  $FILE
-echo "object-width:${WIDTH}px"  								>>  $FILE
-echo "object-zindex:100" 									>>  $FILE
-echo "text-background-color:rgb(${BOXCOLOR})"  		>>  $FILE
-echo "text-font-family:'Courier New',Courier,monospace"  >>  $FILE
-#echo "text-padding-x:2px"												>>  $FILE
-#echo "text-padding-y:2px"										>>  $FILE
-echo "text-font-color:rgb(0, 0, 0)"							>>  $FILE
-echo "text-font-size:13px"										>>  $FILE
-echo ""															>>  $FILE
-
-cat ${FILE}.tmp | grep -v ^% | pandoc -r markdown -w html >> $FILE
-rm ${FILE}.tmp
-
-
-
-
- 	
- FILE=`expr $FILE + 1`
-
-
-
-echo "put $FILE $hotglueContent/${FILE}" >> ftp.temp
-echo "chmod 666 $hotglueContent/${FILE}" >> ftp.temp
-
-
-
-
-
-
-
-
-
-
-
-echo "$FILE"
-
-
+     # START NEW FILE
+       FILENAME=`expr $FILENAME + 1`
+       FILE=$TMPDIR/$FILENAME
  
- fi  
+      fi  
+ 
+   done
+# --------------------------------------------------------------------------- #
+
+# --------------------------------------------------------------------------- #
+# MAKE HOTGLUE FILES/BOXES FOR SEPARATE PAŔTS
+# --------------------------------------------------------------------------- #
+
+  for BOX in `ls $TMPDIR/*.box`
+   do
+
+      FILE=${BOX%%.*}
+
+      MAXPERLINE=50
+      YPOS=$(python -c "print ${YPOS}+${HEIGHT}+100")
+      LINENUMBER=`cat $BOX | grep -v ^% | fold -s -w $MAXPERLINE | wc -l`
+      HEIGHT=`expr $LINENUMBER \* 15`
+
+      BOXCOLOR=$((RANDOM%255))","$((RANDOM%255))","$((RANDOM%255))
+  
+    # WRITE HOTGLUE HEADER
+      echo "type:text"                                          >  $FILE
+      echo "module:text"                                        >> $FILE
+      echo "object-height:${HEIGHT}px"                          >> $FILE
+      echo "object-left:${XPOS}px"                              >> $FILE
+      echo "object-top:${YPOS}px"                               >> $FILE
+      echo "object-width:${WIDTH}px"                            >> $FILE
+      echo "object-zindex:100"                                  >> $FILE
+      echo "text-background-color:rgb(${BOXCOLOR})"             >> $FILE
+      echo "text-font-family:'Courier New',Courier,monospace"   >> $FILE
+      echo "text-padding-x:2px"                                 >> $FILE
+      echo "text-padding-y:2px"                                 >> $FILE
+      echo "text-font-color:rgb(0, 0, 0)"                       >> $FILE
+      echo "text-font-size:13px"                                >> $FILE
+      echo ""                                                   >> $FILE
+ 
+    # CONVERT COLLECTED LINES TO HTML AND ATTACH TO FILE
+      cat ${BOX} | \
+      grep -v ^% | \
+      pandoc -r markdown -w html                                >> $FILE
+
+      rm ${BOX}
+
+      HOTGLUEFILE=`basename $FILE`
+    # UPLOAD VIA FTP (CONTROL FILE USED LATER)
+      echo "put $FILE $HOTGLUECONTENT/${HOTGLUEFILE}" >> $FTPTMP
+    # CHMOD TO MAKE EDITABLE VIA HOTGLUE/BROWSER
+      echo "chmod 666 $HOTGLUECONTENT/${HOTGLUEFILE}" >> $FTPTMP
+
+  done
 
 
 
-  echo "$LINE"  >>  ${FILE}.tmp
+  echo "bye"  >>  $FTPTMP
+  ftp -i -p -n $HOST < $FTPTMP
+
+# CLEAN UP
+  rm $TMPDIR/*.*
 
 
-done
-
-
-
-
-
-
-
-
-echo "bye"  >>ftp.temp
-
-ftp -i -p -n $HOST < ftp.temp
-
-exit 0
+exit 0;
