1e81fc172b9aeb4221f6406fbb1f41c91ab7ea2e
christop <christoph@lafkon.net>
2014-06-25 13:21:10 +0200

    remove footnotes and citations

--- pad2hotglue.sh
+++ pad2hotglue.sh
@@ -16,8 +16,7 @@
 # UPLOAD VIA FTP (CONTROL FILE USED LATER)
   echo "$ACCESS"                    >  $FTPTMP
   echo "mdelete $HOTGLUECONTENT/1*" >> $FTPTMP
- 
- 
+  
   PADDUMP=$TMPDIR/pad.md
   PADURL=http://note.pad.constantvzw.org:8000/p/interviews.tools-of-the-trade/export/txt
   PADHTML=$TMPDIR/pad.html
@@ -27,12 +26,17 @@
   pandoc tmp/pad.md -r markdown  -w html -o tmp/pad.html
    
   sed -i 's/% NOWSPEAKING:/% NOWSPEAKING: \n/g'  $PADHTML
+
+
+# REMOVE FOOTNOTE
+  sed -i 's/\[\^\][^}]*}//g' $PADHTML
+# REMOVE CITATION REFERENCE
+  sed -i 's/\[@[^]]*]//g' $PADHTML
+
  	
   FILENAME=1000
   FILE=$TMPDIR/$FILENAME
   if [ -f $FILE ]; then rm $FILE ; fi
-
-
 
 # --------------------------------------------------------------------------- #
 # SPLIT THE CONTENT IN SEPARATE PARTS FOR SPEAKERS
@@ -70,14 +74,14 @@
    do
 		
       FILE=${BOX%%.*} 
-		sed -i '/<p>%/d' $BOX
-     	MAXPERLINE=72
+      sed -i '/<p>%/d' $BOX
+      MAXPERLINE=72
       YPOS=$(python -c "print ${YPOS}+${HEIGHT}+30")
       LINENUMBER=`cat $BOX | fold -s -w $MAXPERLINE | wc -l`
       HEIGHT=`expr $LINENUMBER \* 15`
-		RL=$(grep RL $BOX)
-		FS=$(grep FS $BOX)
-		XGUY=$(grep X $BOX)
+      RL=$(grep RL $BOX)
+      FS=$(grep FS $BOX)
+      XGUY=$(grep X $BOX)
       BOXCOLOR=$((RANDOM%255))","$((RANDOM%255))","$((RANDOM%255))
       
       if [ -n "$FS" ]
@@ -106,33 +110,37 @@
     # WRITE HOTGLUE HEADER
       echo "type:text"                                          >  $FILE
       echo "module:text"                                        >> $FILE
-      echo "object-height:${HEIGHT}px"                         >> $FILE
-      echo "object-left:${XPOS}px"                             >> $FILE
-      echo "object-top:${YPOS}px"                              >> $FILE
-      echo "object-width:${WIDTH}px"                           >> $FILE
+      echo "object-height:${HEIGHT}px"                          >> $FILE
+      echo "object-left:${XPOS}px"                              >> $FILE
+      echo "object-top:${YPOS}px"                               >> $FILE
+      echo "object-width:${WIDTH}px"                            >> $FILE
       echo "object-zindex:100"                                  >> $FILE
       echo "text-background-color:rgb(${BOXCOLOR})"             >> $FILE
       echo "text-font-family:'Courier New',Courier,monospace"   >> $FILE
-      echo "text-font-size:12px"											 >> $FILE
-      echo "text-line-height:15px"											 >> $FILE
-      #echo "text-padding-x:2px"                                 >> $FILE
-      #echo "text-padding-y:20px"                                 >> $FILE
+      echo "text-font-size:12px"                                >> $FILE
+      echo "text-line-height:15px"                              >> $FILE
+    # echo "text-padding-x:2px"                                 >> $FILE
+    # echo "text-padding-y:20px"                                >> $FILE
       echo "text-font-color:rgb(0, 0, 0)"                       >> $FILE
       echo "text-font-size:13px"                                >> $FILE
       echo ""                                                   >> $FILE
  
     # CONVERT COLLECTED LINES TO HTML AND ATTACH TO FILE
       cat ${BOX}| \
-      sed -e 's/% ----------------------------//g' -e 's/<blockquote>/<i>/' -e 's/<\/blockquote>/<\/i>/' -e 's/<strong>/<b>/' -e 's/<\/i>\n<\/p>/<\/i><\/p>/' -e 's/<\/p>\n<i>/<p><i>/' -e 's/<\/strong>/<\/b>/' | \
+      sed -e 's/% ----------------------------//g' \
+          -e 's/<blockquote>/<i>/' -e 's/<\/blockquote>/<\/i>/' \
+          -e 's/<strong>/<b>/' -e 's/<\/i>\n<\/p>/<\/i><\/p>/' \
+          -e 's/<\/p>\n<i>/<p><i>/' -e 's/<\/strong>/<\/b>/' | \
       sed  ':a;N;$!ba;s/>\n/>/g'                          >> $FILE  
+
      # pandoc -r markdown  -w html                             
 
-		if [ `ls -a $BOX | grep "^" $BOX` ]
-		then 
-		echo "FOUND"
-		else
-		echo "MISSED, MOTHERFUCKER"
-		fi
+#            if [ `ls -a $BOX | grep "^" $BOX` ]
+#            then 
+#            echo "FOUND"
+#            else
+#            echo "MISSED, MOTHERFUCKER"
+#            fi
 
       rm ${BOX}
 
@@ -147,8 +155,8 @@
 
 
 
-  echo "bye"  >>  $FTPTMP
-  ftp -i -p -n $HOST < $FTPTMP
+  echo "bye"         >> $FTPTMP
+  ftp -i -p -n $HOST  < $FTPTMP
 
 # CLEAN UP
    rm $TMPDIR/*.*
